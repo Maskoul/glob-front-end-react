3cb8c70c1fc43877fb3beea4ae79ffa1630d9f7e
Moe <moha1058@gmail.com>
2020-07-17 19:15:25 +0100

    Add routes for register and home pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,15 +4,36 @@ import Navigation from '../navigation/Navigation';
 import HomePage from '../homePage/HomePage';
 import Register from '../register/Register';
 
-function App() {
-  return (
-    <div>
-        <Navigation />
-{/*        <HomePage />
-*/} 
-        <Register />   
-    </div> 
-  );
+
+const initialState = {
+          route: 'register',
+          isSignIn:false,
+          user : {
+              uid: '',
+              firstName: '',
+              lastName:'',
+              email: '',
+              birthDay:'',
+              gender:'',
+              joined: ''}
+}
+
+class App extends React.Component {
+  
+  state =   initialState
+  
+  
+  render() {
+
+    return (
+      <div>
+          <Navigation isSignIn={this.state.isSignIn} />
+
+          {this.state.route === 'home' && <HomePage />}
+   
+          {this.state.route === 'register' && <Register />}   
+      </div> 
+    )}
 }
 
 export default App;
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -3,23 +3,32 @@ import React from 'react';
 import './Navigation.css';
 import LogIn from '../login/LogIn';
 
-const Navigation = () => {
+const Navigation = (props) => {
+
     return (
-        <div className='nav_container'>
+
+        <nav className='nav_container'>
             <div className='logo'>
                 Glob
             </div>
-           {/* <div className='nav_bar_components flex_nav_items'>
-                <span className='nav_item'>Home</span>
-                <span className='nav_item'>Profile</span>            
-            </div>
-            <div className='flex_log_out_button'>
-                <button className='log_out_button'>Log out</button>
-    </div>*/}
-    <LogIn />
+           
+            {
+                props.isSignIn ? 
+                <div  className='flex_nav_items'>
+                <div className='nav_bar_components'>
+                    <span className='nav_item'>Home</span>
+                    <span className='nav_item'>Profile</span>            
+                </div>
+                <div className='flex_log_out_button'>
+                    <button className='log_out_button'>Log out</button>
+                </div>
+                </div>
+            :
+                <LogIn />
+        }
 
             
-        </div>
+        </nav>
     )
 }
 
